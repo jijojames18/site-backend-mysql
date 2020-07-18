504dfa7374ac552a007d22116858e652e6b102ff
Jijo James <jijojames18@gmail.com>
2020-07-18 19:42:40 +0530

    Upgrade script : Initial version

--- db_upgrade.sh
+++ db_upgrade.sh
@@ -4,7 +4,7 @@ HOST='localhost'
 PORT=3306
 DB='mydb'
 USER='root'
-PASS=''
+PASS=""
 
 while [ "$1" ];
 do
@@ -33,6 +33,30 @@ done
 echo -e "\n"
 echo "Connecting to DB $DB at $HOST:$PORT as $USER"
 
+if [ -z ${PASSWORD+x} ]
+then
+  mysql_connect="mysql $DB -u $USER -h $HOST"
+else
+  mysql_connect="mysql $DB -u $USER -h $HOST -p $PASSWORD"   
+fi
 
+tables_list_query="$mysql_connect -se 'SHOW TABLES'"
+tables_list=$(eval $tables_list_query)
 
+has_upgrade_table=$(echo ${tables_list[@]} | grep -o "upgrade_history" | wc -w)
+
+if [ $has_upgrade_table == 1 ]
+then
+  echo "An existing DB is present"
+else
+  echo "Recreating DB from start"
+  for folder in `ls sql`; do
+    for file in `ls sql/$folder`; do
+      file_path="sql/$folder/$file"
+      echo "***$file_path***"
+      exec_query="$mysql_connect < $file_path"
+      $(eval $exec_query)
+    done
+  done
+fi
 
