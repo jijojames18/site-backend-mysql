1da4b43daa6bc730e77243b5e14411a38e3d190e
Jijo James <jijojames18@gmail.com>
2020-07-19 12:01:58 +0530

    DB upgrade script

--- db_upgrade.sh
+++ db_upgrade.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-HOST='localhost'
+function exec_mysql_query()
+{
+  result=$($1)
+  if [ $? != 0]
+  then
+    exit $?
+  fi
+
+  return result
+}
+
+HOST="localhost"
 PORT=3306
-DB='mydb'
-USER='root'
+DB="mydb"
+USER="root"
 PASS=""
 
 while [ "$1" ];
 do
   arg=$1
   shift
-  value=
-  case $arg in 
+  case $arg in
     "-h")
       HOST=$1
       ;;
@@ -26,7 +36,7 @@ do
       ;;
     "-u")
       USER=$1
-      ;;  
+      ;;
   esac
 done
 
@@ -36,27 +46,65 @@ echo "Connecting to DB $DB at $HOST:$PORT as $USER"
 if [ -z ${PASSWORD+x} ]
 then
   mysql_connect="mysql $DB -u $USER -h $HOST"
+  mysql_check_db="mysql -u $USER -h $HOST"
 else
-  mysql_connect="mysql $DB -u $USER -h $HOST -p $PASSWORD"   
+  mysql_connect="mysql $DB -u $USER -h $HOST -p $PASSWORD"
+  mysql_check_db="mysql -u $USER -h $HOST -p $PASSWORD"
 fi
 
-tables_list_query="$mysql_connect -se 'SHOW TABLES'"
-tables_list=$(eval $tables_list_query)
+db_list=$($mysql_check_db -se 'SHOW DATABASES')
+has_required_db=$(echo ${db_list[@]} | grep -o "$DB" | wc -w)
+if [ $has_required_db == 0 ]
+then
+  echo "Creating database $DB"
+  $($mysql_check_db -se 'CREATE DATABASE $DB')
+fi
 
+tables_list=$($mysql_connect -se 'SHOW TABLES')
 has_upgrade_table=$(echo ${tables_list[@]} | grep -o "upgrade_history" | wc -w)
-
+final_version="v1"
 if [ $has_upgrade_table == 1 ]
 then
   echo "An existing DB is present"
+  last_executed_file=$($mysql_connect -se 'SELECT * FROM upgrade_history')
+  last_folder=$(echo "$last_executed_file" | cut -d "/" -f 2)
+  last_file=$(echo "$last_executed_file" | cut -d "/" -f 3)
+  last_version="${last_folder:1}"
+  last_file_version=$(echo "$last_file" | cut -d "-" -f 1)
+  for current_folder in `ls sql`; do
+    current_version="${current_folder:1}"
+    if [ $current_version -ge $last_version ]
+    then
+      final_version=$current_folder
+      for current_file in `ls sql/$current_folder`; do
+        current_file_version=$(echo "$current_file" | cut -d "-" -f 1)
+        if [ $current_file_version -gt $last_file_version ]
+        then
+          file_path="sql/$current_folder/$current_file"
+          echo "***$file_path***"
+          test=$($mysql_connect < $file_path)
+          if [ $file_path != "sql/v1/001-initial.sql" ]
+          then
+            $($mysql_connect -se 'TRUNCATE upgrade_history; INSERT INTO upgrade_history VALUES(\"$file_path\")')
+          fi
+        fi
+      done
+    fi
+  done
 else
   echo "Recreating DB from start"
   for folder in `ls sql`; do
     for file in `ls sql/$folder`; do
+      final_version=$folder
       file_path="sql/$folder/$file"
       echo "***$file_path***"
-      exec_query="$mysql_connect < $file_path"
-      $(eval $exec_query)
+      $($mysql_connect < $file_path)
+      if [ $file_path != "sql/v1/001-initial.sql" ]
+      then
+        $($mysql_connect -se 'TRUNCATE upgrade_history; INSERT INTO upgrade_history VALUES(\"$file_path\")')
+      fi
     done
   done
 fi
 
+echo "Database upgraded to version $final_version"
